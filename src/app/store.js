bb68e239e57ab53946eb6ad317e3ddb4791f60fc
Michael Kipchumba <michaelchumba09@gmail.com>
2020-05-07 18:34:19 +0300

    feat: create wrapper for fetch api

--- src/app/store.js
+++ src/app/store.js
@@ -2,5 +2,6 @@ import { createStore, combineReducers } from 'redux'
 import { AuthReducer } from './reducers/auth.reducer'
 
 export const store = createStore(
+    // Add your reducer functions to the combineReducers object.
     combineReducers({ AuthReducer })
 )
